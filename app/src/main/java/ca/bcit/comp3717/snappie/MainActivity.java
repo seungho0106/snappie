1b9983a00c5986aab8ea787e40869a54d3dffcc6
David Han <seunghohan16@gmail.com>
2020-11-18 01:11:50 -0800

    configured BottomNavigationView with Navigation component

--- app/src/main/java/ca/bcit/comp3717/snappie/MainActivity.java
+++ app/src/main/java/ca/bcit/comp3717/snappie/MainActivity.java
@@ -4,13 +4,34 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+/**
+ * Hosts {@link androidx.fragment.app.FragmentContainerView} which supports
+ * the {@link androidx.navigation.NavHost} component and fragment-based,
+ * single-activity architecture.
+ *
+ * @author David Han (A01183937)
+ * @version 2020
+ */
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupNavigation();
     }
 
+    private void setupNavigation() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                navHostFragment.getNavController());
+    }
 }
