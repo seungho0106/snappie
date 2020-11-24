1b016c3ace73a98ef785d39b2852f8d94bce4cda
Joshua Shin <joshua_shin@hotmail.com>
2020-11-24 15:23:10 -0800

    Setup firebase and image uploading function

--- app/src/main/java/ca/bcit/comp3717/snappie/ExploreFragment.java
+++ app/src/main/java/ca/bcit/comp3717/snappie/ExploreFragment.java
@@ -1,10 +1,30 @@
 package ca.bcit.comp3717.snappie;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +43,8 @@ public class ExploreFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private ImageView imageViewTest;
+    private FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -58,6 +80,54 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+
+        // Write ImageView to firebase example
+        imageViewTest = view.findViewById(R.id.imageViewTest);
+        imageViewTest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                writeImageViewToFirebase(imageViewTest);
+            }
+        });
+
+        return view;
+    }
+
+    private void writeImageViewToFirebase(ImageView imageView) {
+        // Generate data
+        Bitmap capture = Bitmap.createBitmap(
+                imageView.getWidth(),
+                imageView.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas captureCanvas = new Canvas(capture);
+        imageView.draw(captureCanvas);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        capture.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+        byte[] data = outputStream.toByteArray();
+
+        // Create upload task
+        String path = getStoragePath() + "/" + UUID.randomUUID() + ".png";
+        StorageReference storageReference = firebaseStorage.getReference(path);
+        StorageMetadata metadata = new StorageMetadata.Builder()
+                .setCustomMetadata("caption", "some caption here")
+                .build();
+        UploadTask uploadTask = storageReference.putBytes(data, metadata);
+
+        // Handle success
+        uploadTask.addOnCompleteListener(getActivity(), new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                Toast.makeText(getContext(), "Test.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /* Creates proper path the image is stored in the firebase, according to today's date.
+    * IE. if today's date is 11/24/2020, the image should be stored in directory /11.24.2020/ */
+    private String getStoragePath() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now).replace("/", ".");
     }
 }
