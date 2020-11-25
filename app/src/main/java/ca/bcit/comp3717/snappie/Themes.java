0f764c531505371187ddf774cff4a6d5060a80fa
Joshua Shin <joshua_shin@hotmail.com>
2020-11-24 23:19:19 -0800

    Update themes

--- app/src/main/java/ca/bcit/comp3717/snappie/Themes.java
+++ app/src/main/java/ca/bcit/comp3717/snappie/Themes.java
@@ -9,12 +9,14 @@ public class Themes {
         put("11.22.2020", "The nicest tree you can find!");
         put("11.23.2020", "A good dog.");
         put("11.24.2020", "Most precious cat!");
-        put("11.25.2020", "What's on your table?");
-        put("11.26.2020", "Your favorite pen.");
-        put("11.27.2020", "Book you read recently.");
+        put("11.25.2020", "The coolest cloud!");
+        put("11.26.2020", "Greenest green.");
+        put("11.27.2020", "Nice place to sit.");
         put("11.28.2020", "Sunshine.");
-        put("11.29.2020", "Something delicious!.");
-        put("11.30.2020", "How about a fruit?");
+        put("11.29.2020", "Local store front.");
+        put("11.30.2020", "A street lamp.");
         put("12.01.2020", "The cold December air.");
+        put("12.02.2020", "Nice place to walk.");
+        put("12.03.2020", "Your local park.");
     }};
 }
